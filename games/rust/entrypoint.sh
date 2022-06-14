be62597df6be4eb8eb81ef1c4bee26dc4e1ec3b4
Joshua Schmitt <jqshuv@gmail.com>
2022-06-14 18:33:33 +0000

    added source games image

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -15,7 +15,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "\033[1m\033[33mequestrian@realms~ \033[0m%s\n" "${MODIFIED_STARTUP}"
 
 # OxideMod has been replaced with uMod
 if [ -f OXIDE_FLAG ] || [ "${OXIDE}" = 1 ] || [ "${UMOD}" = 1 ]; then
