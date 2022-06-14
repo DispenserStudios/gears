9fff401503c7f086c0596e9aa1d82fb8c3ac9533
Joshua Schmitt <jqshuv@gmail.com>
2022-06-14 17:00:02 +0000

    fix

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -9,24 +9,23 @@
 TZ=${TZ:-UTC}
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-# Print Java version
-printf "\033[1m\033[33mequestrian@realms~ \033[0mjava -version\n"
-java -version
+# Print Node.js Version
+printf "\033[1m\033[33mequestrian@realms~ \033[0mnode -vn\n"
+node -v
 
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "\033[1m\033[33mequestrian@realms~ \033[0m%s\n" "$MODIFIED_STARTUP"
-# shellcheck disable=SC2086
-exec env ${MODIFIED_STARTUP}
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
